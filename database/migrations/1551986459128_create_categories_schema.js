472912e588671af8b139ccbe95ab56e39bd89ac0
sandgoat <ryankeithgrant@gmail.com>
2019-05-06 15:27:56 -0500

    shoes

---
+++ database/migrations/1551986459128_create_categories_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CreateCategoriesSchema extends Schema {
+  up () {
+    this.raw(
+    	`CREATE TABLE categories(
+		  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		  title VARCHAR(200) NOT NULL,
+		  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE 
+		  CURRENT_TIMESTAMP	
+    	)
+    	`
+    	)
+  }
+
+  down () {
+    this.drop('create_categories')
+  }
+}
+
+module.exports = CreateCategoriesSchema
